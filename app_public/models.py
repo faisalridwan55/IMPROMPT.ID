5e8189d2a0a78e1edd820f00acc877752d6a20be
faisal ridwan <faisalridwan55@gmail.com>
2018-04-18 00:32:06 +0700

    ubah db imprompt.profile

--- app_public/models.py
+++ app_public/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Imprompt_Profile(models.Model):
-    baris_atas = models.TextField(blank=False)
+	baris_atas_judul = models.CharField(max_length=140, blank=False)
+    baris_atas_description = models.TextField(blank=False)
+    baris_tengah_judul = models.CharField(max_length=140, blank=False)
+    baris_tengah = models.TextField(blank=True)
+    baris_bawah_judul = models.CharField(max_length=140, blank=False)
     baris_bawah = models.TextField(blank=True)
-    active = models.BooleanField(default=False, unique=True)
 
 class News(models.Model):
     news_title = models.CharField(max_length=140, blank=False)
